d05afb455c774e5ab86ec59b5d421d407d5cfce2
Carlos Interone <carlos@mostlyclicks.com>
2015-08-09 21:06:01 -0500

    add brands controller

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,11 @@
+class BrandsController < ApplicationController
+  
+  def index
+    @brands = Brand.all
+  end
+
+  def show
+    @brand = Brand.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :brands, only: [:index, :show]
+
 end
