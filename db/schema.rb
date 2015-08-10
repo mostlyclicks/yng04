d02da1249d62d5eed6c830fd837d1a400ee457f4
Carlos Interone <carlos@mostlyclicks.com>
2015-08-09 21:23:44 -0500

    add prod model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810011751) do
+ActiveRecord::Schema.define(version: 20150810022032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,4 +57,15 @@ ActiveRecord::Schema.define(version: 20150810011751) do
   add_index "brands", ["email"], name: "index_brands_on_email", unique: true, using: :btree
   add_index "brands", ["reset_password_token"], name: "index_brands_on_reset_password_token", unique: true, using: :btree
 
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "brand_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "products", ["brand_id"], name: "index_products_on_brand_id", using: :btree
+
+  add_foreign_key "products", "brands"
 end
